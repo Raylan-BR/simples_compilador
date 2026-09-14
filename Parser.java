c3be6020666ddc07c897a21286c78198db89a046
Raylan Bruno <raylanbruno2002@gmail.com>
2026-09-14 12:52:02 -0300

    Atualizando Parser para suportar number

--- Parser.java
+++ Parser.java
@@ -1,7 +1,7 @@
-public class Parser {
+class Parser {
     
     private Scanner scan;
-    private char currentToken;
+    private Token currentToken;
 
     public Parser(byte[] input) {
         scan = new Scanner(input);
@@ -10,43 +10,36 @@ public class Parser {
     private void nextToken () {
     currentToken = scan.nextToken();
     }
-    private void match(char t) {
-        if (currentToken == t) {
+
+    private void match(TokenType t) {
+        if (currentToken.type == t) {
             nextToken();
         }else {
             throw new Error("syntax error");
         }
+   }
+    void number () {
+        System.out.println("push " + currentToken.lexeme);
+        match(TokenType.NUMBER);
     }
-    void digit () {
-        if (Character.isDigit(currentToken)) {
-						System.out.println("push " + currentToken);
-            match(currentToken);
-        } else {
-           throw new Error("syntax error");
-        }
+       void expr() {
+        number();
+        oper();
     }
     void oper () {
-        if (currentToken == '+') {
-            match('+');
-            digit();
+        if (currentToken.type == TokenType.PLUS) {
+            match(TokenType.PLUS);
+            number();
             System.out.println("add");
             oper();
-        } else if (currentToken == '-') {
-            match('-');
-            digit();
+        } else if (currentToken.type == TokenType.MINUS) {
+            match(TokenType.MINUS);
+            number();
             System.out.println("sub");
             oper();
-        }
-    }
-    void expr() {
-        digit();
-        oper();
+        } 
     }
     public void parse () {
         expr();
     }
 }
-
-  
-
-		
