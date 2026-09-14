f1e9a71dd682b4b4963d77971e08aefdd1d1ab04
Raylan Bruno <raylanbruno2002@gmail.com>
2026-09-13 23:39:52 -0300

    teste: imprimir token + lexema

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-				String input = "289-85+0+69";
-        Scanner scan = new Scanner (input.getBytes());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-}
+  public static void main(String[] args) {
+          String input = "289-85+0+69";
+          Scanner scan = new Scanner (input.getBytes());
+          System.out.println(scan.nextToken());
+          System.out.println(scan.nextToken());
+          System.out.println(scan.nextToken());
+          System.out.println(scan.nextToken());
+          System.out.println(scan.nextToken());
+          System.out.println(scan.nextToken());
+          System.out.println(scan.nextToken());
+  }
 }
