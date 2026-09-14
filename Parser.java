b9d21d9ca8663d23fa1d7ba80b702fa1e61ab7a1
Raylan Bruno <raylanbruno2002@gmail.com>
2026-09-13 23:23:40 -0300

    Analisador Léxico: passo 2 e 3

--- Parser.java
+++ Parser.java
@@ -1,52 +1,52 @@
 public class Parser {
-    private byte[] input;
-    private int current; 
+    
+    private Scanner scan;
+    private char currentToken;
 
-	public Parser (byte[] input) {
-        this.input = input;
+    public Parser(byte[] input) {
+        scan = new Scanner(input);
+        currentToken = scan.nextToken();
     }
-
-    public void parse () {
-        expr();
+    private void nextToken () {
+    currentToken = scan.nextToken();
     }
-     private char peek () {
-        if (current < input.length)
-           return (char)input[current];
-       return '\0';
-    }
-    private void match (char c) {
-        if (c == peek()) {
-            current++;
-        } else {
+    private void match(char t) {
+        if (currentToken == t) {
+            nextToken();
+        }else {
             throw new Error("syntax error");
         }
-  }
-    void expr() {
-            digit();
-            oper();
     }
     void digit () {
-        if (Character.isDigit(peek())) {
-            System.out.println("push " + peek());
-            match(peek());
+        if (Character.isDigit(currentToken)) {
+						System.out.println("push " + currentToken);
+            match(currentToken);
         } else {
            throw new Error("syntax error");
         }
     }
-
-
-	void oper () {
-        if (peek() == '+') {
+    void oper () {
+        if (currentToken == '+') {
             match('+');
             digit();
             System.out.println("add");
             oper();
-        } else if (peek() == '-') {
+        } else if (currentToken == '-') {
             match('-');
             digit();
             System.out.println("sub");
             oper();
-        } 
+        }
     }
-
+    void expr() {
+        digit();
+        oper();
+    }
+    public void parse () {
+        expr();
+    }
 }
+
+  
+
+		
