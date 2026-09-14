987d5ec776fb27a11d07207495312dc9c3c6b8ae
Raylan Bruno <raylanbruno2002@gmail.com>
2026-09-14 12:54:55 -0300

    método para remover espaços em branco

--- Scanner.java
+++ Scanner.java
@@ -18,7 +18,11 @@ public class Scanner {
         }
     }
     public Token nextToken () {
+
+        skipWhitespace();
+
         char ch = peek();
+
         if (ch == '0') {
             advance();
             return new Token (TokenType.NUMBER, Character.toString(ch));
@@ -48,4 +52,12 @@ public class Scanner {
             String n = new String(input, start, current-start)  ;
             return new Token(TokenType.NUMBER, n);
         }
+    
+    private void skipWhitespace() {
+        char ch = peek();
+        while (ch == ' ' || ch == '\r' || ch == '\t' || ch == '\n') {
+            advance();
+            ch = peek();
+        }
+    }
 }
